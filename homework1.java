ddfb51477a7684745cf70367bd5728cb5188df0d
chiwei1121 <chichi11121@hotmail.com.tw>
2014-02-22 11:16:26 +0800

    week1

---
+++ homework1.java
@@ -0,0 +1,10 @@
+public class homework1 {
+  public static void main(String args[])  { 
+  
+	System.out.println("秋夕 唐．杜牧\n\n");
+	System.out.println("銀燭秋光冷畫屏，\n");
+	System.out.println("輕羅小扇撲流螢。\n");
+	System.out.println("天階夜色涼如水，\n");
+	System.out.println("臥看牽牛織女星。\n");
+  }
+}
